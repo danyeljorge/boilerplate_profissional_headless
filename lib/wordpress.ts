dc12f16fbd3da762e1f838d5cf9547b887ec0c47
Daniel Jorge <danyeljorge@outlook.com>
2026-07-28 21:55:21 -0300

    fazendo o fetch para buscar novos posts a cada 20segundos

--- lib/wordpress.ts
+++ lib/wordpress.ts
@@ -1,6 +1,28 @@
-import {graphQLClient} from "../lib/graphql";
 import {GET_POSTS} from "../lib/queries";
 
+const API_URL = "https://cms.danieljorge.dev.br/graphql";
+
 export async function getPosts() {
-    return  graphQLClient.request(GET_POSTS);
+    const response = await fetch(API_URL, {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            query: GET_POSTS,
+        }),
+        next: {
+            revalidate: 20,
+        },
+
+
+    
+});
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar posts.`);
+
+            return response.json();
+        }
+
 }
